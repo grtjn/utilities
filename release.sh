f4bdd3e2bfab6704f92a1aa37bd1897c61605ac7
Geert Josten <grtjn@xs4all.nl>
2013-04-03 22:03:09 +0200

    release testing

--- release.sh
+++ release.sh
@@ -13,32 +13,93 @@ DEVVERSION=$2
 echo ""
 echo "Create release branch %RELEASE%.."
 git checkout -b $RELVERSION
+if [ $? -gt 0 ]; then
+	echo "";
+	echo "Release failed!";
+	echo "";
+	exit
+fi
 
 echo ""
 echo "Updating release pom versions.."
 mvn versions:set -DnewVersion=$RELVERSION
+if [ $? -gt 0 ]; then
+	echo "";
+	echo "Release failed!";
+	echo "";
+	exit
+fi
 
 echo ""
 echo "Build CMS release.."
 mvn clean install
+if [ $? -gt 0 ]; then
+	echo "";
+	echo "Release failed!";
+	echo "";
+	exit
+fi
+
+echo ""
+echo "Committing pom changes.."
+git commit -a -m "Release $RELEASE"
+if [ $? -gt 0 ]; then
+	echo "";
+	echo "Release failed!";
+	echo "";
+	exit
+fi
 
 echo ""
 echo "Pushing release to remote.."
-git commit -a -m "Release $RELEASE"
 git push -u origin $RELVERSION
+if [ $? -gt 0 ]; then
+	echo "";
+	echo "Release failed!";
+	echo "";
+	exit
+fi
 
 echo ""
-echo "Switching to development branch.."
+echo "Switching back to development branch.."
 git checkout master
+if [ $? -gt 0 ]; then
+	echo "";
+	echo "Release failed!";
+	echo "";
+	exit
+fi
 
 echo ""
 echo "Updating dev pom versions.."
 mvn versions:set -DnewVersion=$DEVVERSION
+if [ $? -gt 0 ]; then
+	echo "";
+	echo "Release failed!";
+	echo "";
+	exit
+fi
+
+echo ""
+echo "Committing dev pom changes.."
+git commit -a -m "Next development release $DEVVERSION"
+if [ $? -gt 0 ]; then
+	echo "";
+	echo "Release failed!";
+	echo "";
+	exit
+fi
 
 echo ""
 echo "Pushing dev versions to remote.."
-git commit -a -m "Next development release $DEVVERSION"
 git push
+if [ $? -gt 0 ]; then
+	echo "";
+	echo "Release failed!";
+	echo "";
+	exit
+fi
 
-echo Release finished!
+echo ""
+echo "Release succesful!"
 echo ""
